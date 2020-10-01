8ba4e0d845d2003a677f143cbfab09e99d8c3760
Angelica <Angelica137@users.noreply.github.com>
2020-10-01 20:32:30 +0100

    plot the earthquakes

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -1,5 +1,8 @@
 import json
 
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
 # Explore the structure of the data
 filename = 'data/eq_data_1_day_m1.json'
 with open(filename) as f:
@@ -25,3 +28,10 @@ for eq_dict in all_eq_dicts:
 print(mags[:10])
 print(lons[:5])
 print(lats[:5])
+
+# Map the earthquakes
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
